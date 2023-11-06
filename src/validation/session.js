690653b1703915d58f4b9666e088ca7e82167504
Luis-devs <luismonteria2001@gmail.com>
2023-11-06 15:13:01 -0500

    Mejora en la validación de la sesión

--- src/validation/session.js
+++ src/validation/session.js
@@ -1,7 +1,8 @@
 import store from '../store';
 import router from '@/router';
+import Swal from 'sweetalert2';
 class Session {
-    expiredSession() {
+    async expiredSession() {
         const time_day = new Date().getTime();
         const current_login = new Date(store.getters.usuario.hora_login);
         const current_login_sig = current_login;
@@ -9,8 +10,19 @@ class Session {
         //2023-05-12:12:00:00
         if (time_day >= current_login_sig.getTime()) {
             this.clearDataStore();
+            await Swal.fire({
+                icon: 'info',
+                title: 'Sesión expirada',
+                text: 'Tú sesión expiró, vuelve a iniciar sesión',
+                showConfirmButton: false,
+                allowEscapeKey: false,
+                allowOutsideClick: false,
+                timer: 1770
+            });
             router.push('/');
+            return true;
         }
+        return false;
     }
 
     clearDataStore() {
