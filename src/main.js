357709bc800563fca4200c31e11278835a92c8e4
Andres Castillo <Andressempai03@gmail.com>
2023-12-08 14:07:38 -0500

    commit de restuaracion

--- src/main.js
+++ src/main.js
@@ -6,5 +6,5 @@ import createVuetify from './plugins/vuetify'
 import router from './router/index'
 import store from './store';
 
-
+//Montar la app
 createApp(App).use(createVuetify).use(router).use(store).mount('#app')
